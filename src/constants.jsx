440cef0547ae8b234f9159999f16b5419ba9353b
Amilkar Munoz <yellowballpython@gmail.com>
2021-08-05 20:55:12 -0600

    api base url to prod

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8000/api'
+// export const API_BASE_URL = 'http://localhost:8000/api'
+export const API_BASE_URL = 'http://los-junior-backend/api'
 
 export const LOGO_URL =
   'https://user-images.githubusercontent.com/71573508/123221377-1eab8800-d48c-11eb-8a76-34a07a4ae62c.png'
