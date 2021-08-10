8688b6ddda77304cd8af964fd26e5af5a5758f61
Amilkar Munoz <yellowballpython@gmail.com>
2021-08-09 19:51:20 -0600

    add extra props to blogSlice

--- src/components/blog/blogSlice.js
+++ src/components/blog/blogSlice.js
@@ -6,6 +6,8 @@ const initialState = {
     posts: null,
     currentPaginatorPage: 1,
     loading: true,
+    searching: false,
+    searchQueryValue: ""
 }
 
 const blogSlice = createSlice({
@@ -20,18 +22,28 @@ const blogSlice = createSlice({
         },
         setLoading: (state, action) => {
             state.loading = action.payload
-        }
+        },
+        setSearching: (state, action) => {
+            state.searching = action.payload
+        },
+        setSearchQueryValue: (state, action) => {
+            state.searchQueryValue = action.payload
+        },
     }
 })
 
 export const {
     setCurrentPaginatorPage,
     setPosts,
-    setLoading
+    setLoading,
+    setSearching,
+    setSearchQueryValue
 } = blogSlice.actions
 
 export const selectPosts = state => state.blogSlice.posts
 export const selectCurrentPaginatorPage = state => state.blogSlice.currentPaginatorPage
 export const selectPostsLoading = state => state.blogSlice.loading
+export const selectSearching = state => state.blogSlice.searching
+export const selectSearchQueryValue = state => state.blogSlice.searchQueryValue
 
 export default blogSlice.reducer
